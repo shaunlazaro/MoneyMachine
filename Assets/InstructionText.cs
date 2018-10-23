c0a9e3a52e498f5f2989b0540743d5c6b16f6557
Shaun Lazaro <shaunlazaro@gmail.com>
2018-10-22 22:42:28 -0400

    Version 0.1: Initial Commit
    Basic prototype of the game:
    - Passive income, 2 types of upgrades
    - Active income, 1 type of upgrades
    - Lightly developed UI
    - Many bugs
    Full details are written in the docs.

---
+++ Assets/InstructionText.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructionText : MonoBehaviour {
+
+    public GameObject ToolTip;
+    private Text ToolTipText;
+
+    private string printer, upgradePrinter, bank, 
+        investment, investmentUpgrade, blank;
+
+    void Start()
+    {
+        ToolTipText = ToolTip.GetComponent<Text>();
+        printer= "Printer:\n" +
+            "Prints money when you swipe it.";
+        upgradePrinter = "Upgrade Printer: \n" +
+            "Makes your printer give you more money when you smack it.";
+        bank = "Bank:\n" +
+            "The bank spits out money if you have investments.";
+        investment = "Investments:\n" +
+            "Invest money to get some passive income.";
+        investmentUpgrade = "Study Investments:\n" +
+            "Allows you to get more money from your investments.";
+        blank = "Hover your mouse for more instructions!";
+    }
+
+    public void UpdateText(string newText)
+    {
+        switch(newText)
+        {
+            case "printer":
+                ToolTipText.text = printer;
+                break;
+            case "upgradePrinter":
+                ToolTipText.text = upgradePrinter;
+                break;
+            case "bank":
+                ToolTipText.text = bank;
+                break;
+            case "investment":
+                ToolTipText.text = investment;
+                break;
+            case "investmentUpgrade":
+                ToolTipText.text = investmentUpgrade;
+                break;
+            case "blank":
+                ToolTipText.text = blank;
+                break;
+            default:
+                Debug.Log("Invalid Input UpdateText\n"+newText);
+                break;
+        }
+    }
+
+    public void HideText()
+    {
+        UpdateText("blank");
+    }
+}
